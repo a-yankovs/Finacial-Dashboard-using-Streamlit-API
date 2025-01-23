68bf55165578257787d52e85f06028f6920503a7
a-yankovs <150750157+a-yankovs@users.noreply.github.com>
2025-01-22 21:40:19 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
-from pages import commodities, cryptos, etfs_value, underpriced_stocks
+import numpy as np
+import time
+import random
+from pages import commodities, cryptos, etfs_value 
+# underpriced_stocks
 
 # site structure 
 pages = {
     "Commodities": commodities, 
     "Cryptocurrencies": cryptos, 
     "ETFs": etfs_value, 
-    "Stocks": underpriced_stocks
+    # "Stocks": underpriced_stocks
 }
 
 #set up of sidebar navigation in between pages
@@ -17,5 +21,40 @@ if choice != "Home":
 else:
     st.write("# Welcome!")
     st.write("Use the sidebar to navigate between pages.")
+
+
+# very simple chatbot layout - work in progress 
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "What's up"
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages: 
+    with st.chat_message(message["role"]): 
+        st.markdown(message["content"])
+
+prompt = st.chat_input("How can I help?")
+if prompt: 
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"): 
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    
+
+
+     
 
 
